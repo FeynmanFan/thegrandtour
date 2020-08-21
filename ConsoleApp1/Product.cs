c2bfda927ab0084bc04baa60a077e00b35d9ba27
Chris B. Behrens <cbehrens@gmail.com>
2020-08-21 13:11:11 -0500

    Removed bum content

--- ConsoleApp1/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    class Product
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int SpaceInBasket { get; set; }
-    }
-}
