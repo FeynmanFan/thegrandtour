0494a6d46c05a60fb8a01d60339fce668200a76b
Chris B. Behrens <cbehrens@gmail.com>
2020-08-21 13:16:07 -0500

    Added console project

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SpaceInBasket { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product iceCream = new Product
+            {
+                Name = "Ice Cream",
+                Price = 5.99m,
+                SpaceInBasket = 1
+            };
+
+            Product frozenYogurt = new Product
+            {
+                Name = "Frozen Yogurt",
+                Price = 6.99m,
+                SpaceInBasket = 2
+            };
+
+            Buy(iceCream);
+            Buy(frozenYogurt);
+        }
+
+        static void Buy(Product product)
+        {
+            GetFromShelf(product);
+            PlaceInBasket(product);
+            PlaceWithChecker(product);
+            PayForProduct(product);
+        }
+
+        private static void GetFromShelf(Product product)
+        {
+            Console.WriteLine("Get " + product.Name + " from shelf.");
+        }
+
+        private static void PlaceInBasket(Product product)
+        {
+            Console.WriteLine("Place " + product.Name + " in basket, size: " + product.SpaceInBasket);
+        }
+
+        private static void PlaceWithChecker(Product product)
+        {
+            Console.WriteLine($"Place {product.Name} with checker");
+        }
+
+        private static void PayForProduct(Product product)
+        {
+            Console.WriteLine($"Pay for {product.Name}: {product.Price}");
+        }
+    }
+}
